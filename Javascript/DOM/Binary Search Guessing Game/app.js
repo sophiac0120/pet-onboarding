07ee0df0b701f9bdd4aa037f4547af22ffcd834e
Ji Who Choi <jiwhochoi@macbook-pro-2.war.can.ibm.com>
2018-09-25 17:24:51 -0400

    Added play again button to user interface

--- Javascript/DOM/Binary Search Guessing Game/app.js
+++ Javascript/DOM/Binary Search Guessing Game/app.js
@@ -5,13 +5,19 @@ let min = 1,
  
 //rand = Math.floor((Math.random() * 21) + 1);
 
-const game = document.getElementById('#game'),
+
+const game = document.querySelector('.game'),
     minNum = document.querySelector('.min-num'),
     maxNum = document.querySelector('.max-num'),
     guessBtn = document.querySelector('#submit'),
     guessInput = document.querySelector('#guess'),
     message = document.querySelector('.message');
 
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
 
 guessBtn.addEventListener('click', function(e){
     let guess = parseInt(guessInput.value);
@@ -19,22 +25,42 @@ guessBtn.addEventListener('click', function(e){
     console.log(rand);
 
     if (guess === NaN || guess < min || guess > max){
-        notifyUser(`Please enter a number between ${min} and ${max}!`);
+        notifyUser(`Please enter a number between ${min} and ${max}!`, 'red');
     }
     else{
-        
-        if (guess == rand){
-            notifyUser('You made a correct guess');
+        if (guess == rand){ 
+            gameOver(true,'You made a correct guess');
         }
         else {
             guessesLeft -= 1;
-            notifyUser(`Wrong guess! Try again! Your remaining guesses left is ${guessesLeft}`);
+            if (guessesLeft === 0){
+                gameOver(false, 'You used all the remaining guesses');
+            } else {
+                guessInput.style.borderColor = 'red';
+                notifyUser(`Wrong guess!   Try again! Your remaining guesses left is ${guessesLeft}`, 'blue');}
         }
     }
 
     e.preventDefault();
-});
+}); 
 
-function notifyUser(err){
+function notifyUser(err, color){
+    message.style.color = color;  
     message.textContent = err;
+
+}
+
+function gameOver(won, msg){
+    if (won === true){
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        notifyUser(msg, 'green');}
+    else{
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'red';
+        notifyUser(msg, 'black'); 
+    }
+
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
 }
