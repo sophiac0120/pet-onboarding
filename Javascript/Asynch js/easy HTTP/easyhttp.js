9b6130d77d6dc06d2536ea35a3c909e359ee654d
Ji Who Choi <jiwhochoi@macbook-pro-2.war.can.ibm.com>
2018-09-26 16:01:47 -0400

    Added custom POST, PUT, DELETE HTTP Library using Callbacks

--- Javascript/Asynch js/easy HTTP/easyhttp.js
+++ Javascript/Asynch js/easy HTTP/easyhttp.js
@@ -3,17 +3,59 @@ function easyHTTP(){
 }
 
 
-easyHTTP.prototype.get = function(url, callback){
+/* easyHTTP.prototype.get = function(url, callback){
     this.http.open('GET', url, true);
-
     let self = this;
+
     this.http.onload = function() {
+        console.log(self.http.status);
         if (self.http.status === 200){
             callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
         }
     }
 
     this.http.send();
 }
+ */
 
 
+/* easyHTTP.prototype.post = function(url, data, callback){
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    let self = this;
+
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+    this.http.send(JSON.stringify(data));
+}
+ */
+
+/* easyHTTP.prototype.put = function(url, data, callback){
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    let self = this;
+
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+    this.http.send(JSON.stringify(data));
+} */
+
+easyHTTP.prototype.delete = function(url, callback){
+    this.http.open('DELETE', url, true);
+    let self = this;
+
+    this.http.onload = function() {
+        console.log(self.http.status);
+        if (self.http.status === 200){
+            callback(null, 'Post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
