ceee91443f2ee7d9178cc2644a642232cd89f138
Ji Who Choi <jiwhochoi@macbook-pro-2.war.can.ibm.com>
2018-10-02 17:05:49 -0400

    Added Javascript patterns exercises on UICtrl

---
+++ Javascript/js_sandbox_patterns/app.js
@@ -0,0 +1,50 @@
+// (function(){
+
+
+//     return {
+        
+//     }
+// })
+
+
+// const UICtrl = (function(){
+//     let text = 'Hello World';
+
+//     const changeText = function(){
+//         const element = document.querySelector('h1');
+//         element.textContent = text;
+//     }
+
+//     return {
+//         callChangeText: function(){
+//             changeText();
+//         }
+//     }
+// })(); 
+
+// UICtrl.callChangeText();
+
+const ItemCtrl = (function(){
+    let data = [];
+
+    function add(item){
+        data.push(item);
+        console.log('Item Added.....');
+    }
+
+    function get(id){
+        return data.find(item => {
+            return item.id === id;
+        });
+    }
+
+    return {
+        add: add,
+        get: get
+    }
+}
+
+)();
+
+ItemCtrl.add({id: 1, name: 'John'});
+ItemCtrl.add({id:2, name: 'Mark'});
